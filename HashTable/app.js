9d139c5e1d88e7d1fa58dc8a3a35992515ec6e66
Jeevan Jyoti Dash <dashjeevanjyoti04@gmail.com>
2020-12-19 18:52:14 +0530

    added group anagram in hashTable

--- HashTable/app.js
+++ HashTable/app.js
@@ -65,6 +65,30 @@ class HashTable {
 }
 
 const h = new HashTable();
+
+// function sortString(str) {
+//   return str.split("").sort().join("");
+// }
+
+// strs.forEach((s) => {
+//   h._hash(sortString(s));
+// });
+// strs.forEach((s) => {
+//   h.set(sortString(s), s);
+// });
+// var result = [];
+// var temp = [];
+// h.keys().forEach((s) => {
+//   h.get(s).forEach((v) => {
+//     if (v[0] === s) {
+//       temp.push(v[1]);
+//     }
+//   });
+//   result.push([].concat(...temp));
+//   temp = [];
+// });
+// console.log(result);
+
 console.log("Generating Hash Key:- ");
 console.log("white", h._hash("white"));
 console.log("black", h._hash("black"));
@@ -86,3 +110,21 @@ console.log("Get all values");
 console.log(h.values());
 console.log("Get all keys");
 console.log(h.keys());
+
+// Group anagram problem
+const strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
+var groupAnagrams = function (strs) {
+  let map = new Map();
+  for (let str of strs) {
+    const s = uniform(str);
+    let l = map.get(s) || [];
+    l.push(str);
+    map.set(s, l);
+  }
+  return Array.from(map.values());
+};
+
+var uniform = function (str) {
+  return str.split("").sort().join("");
+};
+console.log(groupAnagrams(strs));
